48fbd59da451df441a17074778c3aab9952f0787
tman0004 <tman0004@gmail.com>
2022-11-30 21:45:57 -0800

    Initial cleanup

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -8,7 +8,6 @@
 #include "threads/init.h" // Imports shutdown_power_off() for use in halt()
 
 static void syscall_handler (struct intr_frame *);
-static void find_tid (struct thread *t, void * aux);
 
 static struct thread *matching_thread;
 static tid_t current_tid;
@@ -29,85 +28,38 @@ syscall_handler (struct intr_frame *f UNUSED)
     /* Holds the stack arguments that directly follow the system call. */
     int args[3];
 
-		/* Get the value of the system call (based on enum) and call corresponding syscall function. */
-		switch(*(int *) f->esp)
-		{
-			case SYS_HALT:
-        /* Call the halt() function, which requires no arguments */
-				halt();
-				break;
+	/* Get the value of the system call (based on enum) and call corresponding syscall function. */
+	switch(*(int *) f->esp)
+	{
+		case SYS_HALT:
+		/* Call the halt() function, which requires no arguments */
+			halt();
+			break;
 
-			case SYS_EXIT:
-        /* Exit has exactly one stack argument, representing the exit status. */
-        get_stack_arguments(f, &args[0], 1);
-				/* We pass exit the status code of the process. */
-				exit(args[0]);
-				break;
+		case SYS_EXIT:
+			/* Exit has exactly one stack argument, representing the exit status. */
+			get_stack_arguments(f, &args[0], 1);
+			/* We pass exit the status code of the process. */
+			exit(args[0]);
+			break;
 
-			case SYS_EXEC:
-				/* The first argument of exec is the entire command line text for executing the program */
-				get_stack_arguments(f, &args[0], 1);
-				/* Return the result of the exec() function in the eax register. */
-				f->eax = exec((const char *) args[0]);
-				break;
+		case SYS_WRITE:
+			/* Get three arguments off of the stack. The first represents the fd, the second
+				represents the buffer, and the third represents the buffer length. */
+			get_stack_arguments(f, &args[0], 3);
 
-			case SYS_WAIT:
-        /* The first argument is the PID of the child process
-           that the current process must wait on. */
-				get_stack_arguments(f, &args[0], 1);
-        /* Return the result of the wait() function in the eax register. */
-				f->eax = wait((pid_t) args[0]);
-				break;
+			/* Transform the virtual address for the buffer into a physical address. */
+			args[1] = (int) pagedir_get_page(thread_current()->pagedir, (const void *) args[1]);
 
-			case SYS_CREATE:
-				// puts("halt");
-				break;
+			/* Return the result of the write() function in the eax register. */
+			f->eax = write(args[0], (const void *) args[1], (unsigned) args[2]);
+			break;
 
-			case SYS_REMOVE:
-				// puts("halt");
-				break;
-
-			case SYS_OPEN:
-				// puts("halt");
-				break;
-
-			case SYS_FILESIZE:
-				// puts("halt");
-				break;
-
-			case SYS_READ:
-				// puts("halt");
-				break;
-
-			case SYS_WRITE:
-        /* Get three arguments off of the stack. The first represents the fd, the second
-           represents the buffer, and the third represents the buffer length. */
-        get_stack_arguments(f, &args[0], 3);
-
-        /* Transform the virtual address for the buffer into a physical address. */
-        args[1] = (int) pagedir_get_page(thread_current()->pagedir, (const void *) args[1]);
-
-        /* Return the result of the write() function in the eax register. */
-        f->eax = write(args[0], (const void *) args[1], (unsigned) args[2]);
-        break;
-
-			case SYS_SEEK:
-				// puts("halt");
-				break;
-
-			case SYS_TELL:
-				// puts("halt");
-				break;
-
-			case SYS_CLOSE:
-				// puts("halt");
-				break;
-
-			default:
-        /* If an invalid system call was sent, terminate the program. */
-				exit(-1);
-				break;
-		}
+		default:
+			/* If an invalid system call was sent, terminate the program. */
+			exit(-1);
+			break;
+	}
 }
 
 /* Terminates Pintos, shutting it down entirely (bummer). */
@@ -120,10 +72,10 @@ void halt (void)
    and its status returned to the kernel. */
 void exit (int status)
 {
-	thread_current()->exit_status = status;
+	//thread_current()->exit_status = status;
 	printf("%s: exit(%d)\n", thread_current()->name, status);
-  // sema_up(&thread_current()->being_waited_on);
-  thread_exit ();
+  	// sema_up(&thread_current()->being_waited_on);
+  	thread_exit ();
 }
 
 /* Writes LENGTH bytes from BUFFER to the open file FD. Returns the number of bytes actually written,
@@ -141,35 +93,7 @@ int write (int fd, const void *buffer, unsigned length)
 		/* do something else with other fd */
 	}
 }
-/* Executes the program with the given file name. */
-pid_t exec (const char *file)
-{
-  /* If a null file is passed in, return a -1. */
-	if(*file == NULL)
-	{
-		return -1;
-	}
 
-  /* Get and return the PID of the process that is created. */
-	pid_t child_tid = process_execute(file);
-	return child_tid;
-}
-
-/* If the PID passed in is our child, then we wait on it to terminate before proceeding */
-int wait (pid_t pid)
-{
-	/* If the thread created is a valid thread, then we must disable interupts, and add it to this threads list of child threads. */
-  return process_wait(pid);
-}
-
-bool create (const char *file, unsigned initial_size) {return true;}
-bool remove (const char *file) {return true;}
-int open (const char *file) {return 0;}
-int filesize (int fd) {return 0;}
-int read (int fd, void *buffer, unsigned length) {return 0;}
-void seek (int fd, unsigned position) {}
-unsigned tell (int fd) {return 0;}
-void close (int fd) {}
 
 /* Check to make sure that the given pointer is not in kernel space,
    is not null, and is not an valid page. We must exit the program
@@ -196,14 +120,4 @@ void get_stack_arguments (struct intr_frame *f, int *args, int num_of_args)
       check_valid_addr((const void *) ptr);
       args[i] = *ptr;
     }
-}
-
-/* This function is passed to thread_foreach in order to find the thread
-   that matches a specific tid. */
-static void find_tid (struct thread *t, void * aux)
-{
-  if(current_tid == t->tid)
-  {
-    matching_thread = t;
-  }
 }
